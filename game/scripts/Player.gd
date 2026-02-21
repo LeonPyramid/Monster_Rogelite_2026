26415bf9a1e66cbdc05333ad1a32395fea39a8e3
rdelpy <romain.delpy@u-bordeaux.fr>
2026-02-21 16:29:12 +0100

    dash_player

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -7,6 +7,8 @@ extends CharacterBody3D
 ## it felt better to leave it in.
 @export var acceleration: float = 500.0
 
+@export var dash_speed:float = 100.0
+
 var target_velocity = Vector3.ZERO
 
 
@@ -20,6 +22,10 @@ func _physics_process(delta):
 	var input_dir :=  Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if input_dir != Vector2.ZERO:
 		velocity += Vector3(input_dir.x,0,input_dir.y) * _adjusted_acceleration(delta)
+		if Input.is_action_just_pressed("dash"):
+			print_debug("dashed")
+			velocity += Vector3(input_dir.x,0,input_dir.y) * dash_speed
+	
 	else:
 		# Reduce the length of our velocity vector by a linear amount each frame, based on our
 		# "adjusted acceleration". If this number ever goes into the negative, we'd reverse direction
@@ -28,7 +34,11 @@ func _physics_process(delta):
 		var new_length = max(0, velocity.length() - _adjusted_acceleration(delta))
 		velocity = velocity.normalized() * new_length
 	# Don't exceed the maximum speed
-	velocity = velocity.limit_length(max_speed)
+	if Input.is_action_just_pressed("dash"):
+		velocity = velocity.limit_length(max_speed+dash_speed)
+		
+	else:
+		velocity = velocity.limit_length(max_speed)
 
 	# Moving the Character
 	#velocity = target_velocity
