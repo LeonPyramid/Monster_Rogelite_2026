1bf864f7ba039c19f7e5f6a57ce60a6e72ecae99
rdelpy <romain.delpy@u-bordeaux.fr>
2026-02-21 15:23:51 +0100

    first movement

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,9 +1,30 @@
-extends Node
+extends CharacterBody3D
+
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+
+
+var target_velocity = Vector3.ZERO
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	# We create a local variable to store the input direction.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	#target_velocity = input_dir * speed
+	target_velocity.x = input_dir.x * speed
+	target_velocity.z = input_dir.y * speed
+
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
