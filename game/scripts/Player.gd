c924d0293987fb22b88f19c5846ded4a6281b35b
rdelpy <romain.delpy@u-bordeaux.fr>
2026-02-21 15:52:20 +0100

    player sliding

--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -2,8 +2,10 @@ extends CharacterBody3D
 
 
 # How fast the player moves in meters per second.
-@export var speed = 14
+@export var max_speed = 14
 
+## it felt better to leave it in.
+@export var acceleration: float = 500.0
 
 var target_velocity = Vector3.ZERO
 
@@ -15,17 +17,25 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
-	#target_velocity = input_dir * speed
-	target_velocity.x = input_dir.x * speed
-	target_velocity.z = input_dir.y * speed
-
+	var input_dir :=  Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_dir != Vector2.ZERO:
+		velocity += Vector3(input_dir.x,0,input_dir.y) * _adjusted_acceleration(delta)
+	else:
+		# Reduce the length of our velocity vector by a linear amount each frame, based on our
+		# "adjusted acceleration". If this number ever goes into the negative, we'd reverse direction
+		# and we'd "flicker" back and forth every physics frame, so let's make sure we never
+		# go into the negative.
+		var new_length = max(0, velocity.length() - _adjusted_acceleration(delta))
+		velocity = velocity.normalized() * new_length
+	# Don't exceed the maximum speed
+	velocity = velocity.limit_length(max_speed)
 
 	# Moving the Character
-	velocity = target_velocity
+	#velocity = target_velocity
 	move_and_slide()
 
+func _adjusted_acceleration(delta: float) -> float:
+	return acceleration * delta
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
